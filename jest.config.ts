bd5b6c9b6da5f679a9068326830e3b35d78fa295
Rafaella <gomesrafaella479@gmail.com>
2025-06-13 07:43:27 +0100

    chore: install Zod and Jest dependencies for validation and testing setup

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  preset: "ts-jest",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+};
+
+module.exports = createJestConfig(config);
